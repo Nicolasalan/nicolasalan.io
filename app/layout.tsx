7dd274f25a6e703547a839689fefd0074c7ee24a
Nicolasalan <nicolasalangrotti03@gmail.com>
2024-01-14 14:08:54 -0300

    add package speed vercel

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import clsx from "clsx";
 
 import { ThemeProvider } from "@/components/ThemeProvider";
@@ -43,6 +44,7 @@ export default function RootLayout({
           </div>
         </ThemeProvider>
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
